63130c97f568584ce2712a1ab37b8ca00356aa35
Abdulhaqdev <nadirovabdulhaq@gmail.com>
2025-04-21 16:43:15 +0500

    fix reload

--- app/dashboard/account/page.tsx
+++ app/dashboard/account/page.tsx
@@ -49,7 +49,7 @@ export default function AccountPage() {
 
   useEffect(() => {
     if (!session) {
-      router.push("/");
+      // router.push("/");
     } else {
       // setUserProfile(session.user);
       setEditedUser({
